c86d5ba51f2c46dd706662b4857c7e603e5c58e2
John Barry <john.olivierb@gmail.com>
2024-08-03 13:40:45 -0400

    Basic subscription form and toast message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,19 @@
 import { fetchPosts } from '@/app/lib/posts/actions';
 import PostsList from '@/app/components/PostsList';
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { SubscriptionForm } from '@/app/components/SubscriptionForm'
 
 
 export default async function Home() {
   const recentPosts = await fetchPosts({ limit: '3', orderBy: 'publish_date', });
   return (
-    <main>
-    
+    <main className='max-w-[1440px] mx-auto'>
       <p>This is regular text</p>
-<p className="font-bold">This is bold text</p>
-<p className="font-medium">This is medium text</p>
-<p className="font-bold-condensed">This is bold condensed text</p>
+      <p className="font-bold">This is bold text</p>
+      <p className="font-medium">This is medium text</p>
+      <p className="font-bold-condensed">This is bold condensed text</p>
       <p className="font-mono">This is mono text</p>
-      <Input placeholder='Search' />
-      <Button variant="default">Button</Button>
-
-      <PostsList className="grid grid-cols-3 gap-4" posts={recentPosts}/>
+      <SubscriptionForm />
+      <PostsList className="grid grid-cols-3 gap-4" posts={recentPosts} />
     </main>
   );
 }
